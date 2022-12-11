eed9063ed12e5dcab4fab04b67da14e3a9904501
Rahulbaran <rahulkumar109422@gmail.com>
2022-12-11 17:54:31 +0530

    initial setup of the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Rahul Baranwal</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
+
+process.env.BROWSER = "firefox";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"]
+    })
+  ],
+
+  server: {
+    open: true,
+    port: 5000
+  },
+  preview: {
+    open: true,
+    port: 5050
+  }
+});
