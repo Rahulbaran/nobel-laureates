655d3c1aa41c7e1e35f52f815f4454b5d0ca12b2
Rahulbaran <rahulkumar109422@gmail.com>
2022-12-15 19:37:24 +0530

    style: do styling of about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = function () {
       <div className="about__wrapper flex flex-col align-center">
         <h1>Welcome Everyone</h1>
         <p>Through this app, we will know about our nobel winners</p>
-        <a className="btn primary-btn about-btn" href="#form">
+        <a className="btn primary-btn about-btn" href="#nobel-wrapper">
           Let&apos;s Start
         </a>
       </div>
