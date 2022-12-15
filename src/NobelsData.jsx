c5ecabef012fa18f891586582cc552ae8633f37d
Rahulbaran <rahulkumar109422@gmail.com>
2022-12-15 07:40:12 +0530

    create noble prizes data file
    - NoblesData.jsx contains the data of noble winners

---
+++ src/NobelsData.jsx
@@ -0,0 +1,218 @@
+export const awardYears = [2022, 2021, 2020, 2019, 2018];
+export const awards = [
+  {
+    year: 2022,
+    awards: [
+      {
+        category: "Medicine",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-03",
+        laureates: ["Svante Pääbo"]
+      },
+      {
+        category: "Physics",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-04",
+        laureates: ["Alain Aspect", "John F. Clauser", "Anton Zeilinger"]
+      },
+      {
+        category: "Peace",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-07",
+        laureates: ["Ales Bialiatski"],
+        orgNames: ["Memorial", "Center for Civil Liberties"]
+      },
+      {
+        category: "Literature",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-06",
+        laureates: ["Annie Ernaux"]
+      },
+      {
+        category: "Economic Science",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-10",
+        laureates: ["Ben Bernanke", "Douglas Diamond", "Philip Dybvig"]
+      },
+      {
+        category: "Chemistry",
+        prizeAmount: 10000000,
+        dateAwarded: "2022-10-05",
+        laureates: ["Carolyn Bertozzi", "Morten Meldal", "K. Barry Sharpless"]
+      }
+    ]
+  },
+  {
+    year: 2021,
+    awards: [
+      {
+        category: "Medicine",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-04",
+        laureates: ["David Julius", "Ardem Patapoutian"]
+      },
+      {
+        category: "Physics",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-05",
+        laureates: ["Syukuro Manabe", "Klaus Hasselmann", "Giorgio Parisi"]
+      },
+      {
+        category: "Peace",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-07",
+        laureates: ["Maria Ressa", "Dmitry Muratov"],
+        orgNames: []
+      },
+      {
+        category: "Literature",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-07",
+        laureates: ["Abdulrazak Gurnah"]
+      },
+      {
+        category: "Economic Science",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-11",
+        laureates: ["David Card", "Joshua D. Angrist", "Guido W. Imbens"]
+      },
+      {
+        category: "Chemistry",
+        prizeAmount: 10000000,
+        dateAwarded: "2021-10-06",
+        laureates: ["Benjamin List", "David W.C. MacMillan"]
+      }
+    ]
+  },
+  {
+    year: 2020,
+    awards: [
+      {
+        category: "Medicine",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-05",
+        laureates: ["Harvey J. Alter", "Michael Houghton", "Charles M. Rice"]
+      },
+      {
+        category: "Physics",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-06",
+        laureates: ["Roger Penrose", "Reinhard Genzel", "Andrea Ghez"]
+      },
+      {
+        category: "Peace",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-09",
+        laureates: [],
+        orgNames: ["World Food Programme"]
+      },
+      {
+        category: "Literature",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-08",
+        laureates: ["Louise Glück"]
+      },
+      {
+        category: "Economic Science",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-12",
+        laureates: ["Paul R. Milgrom", "Robert B. Wilson"]
+      },
+      {
+        category: "Chemistry",
+        prizeAmount: 10000000,
+        dateAwarded: "2020-10-07",
+        laureates: ["Emmanuelle Charpentier", "Jennifer A. Doudna"]
+      }
+    ]
+  },
+  {
+    year: 2019,
+    awards: [
+      {
+        category: "Medicine",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-07",
+        laureates: [
+          "William G. Kaelin Jr",
+          "Sir Peter J. Ratcliffe",
+          "Gregg L. Semenza"
+        ]
+      },
+      {
+        category: "Physics",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-08",
+        laureates: ["James Peebles", "Michel Mayor", "Didier Queloz"]
+      },
+      {
+        category: "Peace",
+        prizeAmount: 9000000,
+        dateAwarded: "2020-10-09",
+        laureates: ["Abiy Ahmed Ali"],
+        orgNames: []
+      },
+      {
+        category: "Literature",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-10",
+        laureates: ["Peter Handke"]
+      },
+      {
+        category: "Economic Science",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-14",
+        laureates: ["Abhijit Banerjee", "Esther Duflo", "Michael Kremer"]
+      },
+      {
+        category: "Chemistry",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-09",
+        laureates: ["John B. Goodenough", "Akira Yoshino"]
+      }
+    ]
+  },
+  {
+    year: 2018,
+    awards: [
+      {
+        category: "Medicine",
+        prizeAmount: 9000000,
+        laureates: ["James P. Allison", "Tasuku Honjo"]
+      },
+      {
+        category: "Physics",
+        prizeAmount: 9000000,
+        dateAwarded: "2018-10-02",
+        laureates: ["Arthur Ashkin", "Gérard Mourou", "Donna Strickland"]
+      },
+      {
+        category: "Peace",
+        prizeAmount: 9000000,
+        laureates: ["Denis Mukwege", "Nadia Murad"],
+        orgNames: []
+      },
+      {
+        category: "Literature",
+        prizeAmount: 9000000,
+        dateAwarded: "2019-10-10",
+        laureates: ["Olga Tokarczuk"]
+      },
+      {
+        category: "Economic Science",
+        prizeAmount: 9000000,
+        laureates: ["William D. Nordhaus", "Paul M. Romer"]
+      },
+      {
+        category: "Chemistry",
+        prizeAmount: 9000000,
+        dateAwarded: "2018-10-03",
+        laureates: [
+          "Frances H. Arnold",
+          "George P. Smith",
+          "Sir Gregory P. Winter"
+        ]
+      }
+    ]
+  }
+];
